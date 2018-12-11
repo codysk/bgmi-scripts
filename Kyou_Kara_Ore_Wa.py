2072cccf7caa0fefadb37ba4cfa6af5ddbd4e1eb
Skywind <hailan@tiantianshiwan.com>
2018-12-11 14:55:40 +0800

    add Kyou_Kara_Ore_Wa

---
+++ Kyou_Kara_Ore_Wa.py
@@ -0,0 +1,34 @@
+#!/usr/pin/python
+# coding=utf-8
+from __future__ import print_function, unicode_literals
+
+import datetime, re, os, imp
+from bgmi.config import SCRIPT_PATH
+from bgmi.lib.fetch import DATA_SOURCE_MAP
+file, pathname, desc = imp.find_module('script_extend', [os.path.join(SCRIPT_PATH)])
+imp.load_module('script_extend', file, pathname, desc)
+from script_extend.script import SearchScriptBase
+
+
+class Script(SearchScriptBase):
+    class Model(SearchScriptBase.Model):
+        bangumi_name = '我是大哥大！！'
+        cover = 'http://lain.bgm.tv/pic/cover/l/c4/16/254821_shoDi.jpg'
+        update_time = 'Sun'
+        due_date = datetime.datetime(2019, 1, 1)
+        source = 'Zimuzu_id'
+        keyword = '37031'
+
+        include_regex_filters = [
+            # r'(BIG5|繁体|繁體|\[繁\])',
+        ];
+
+        exclude_regex_filters = [
+            # r'(HEVC|MKV|H265)',
+        ];
+    
+
+if __name__ == '__main__':
+    os.environ["TEST_RUN"] = '1'
+    s = Script()
+    print(s.get_download_url())
