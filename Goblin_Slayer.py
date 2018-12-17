92cc92c1e3c5a855733cc1b8576a3c77cab69abf
Tachibana Kanade <codysk@users.noreply.github.com>
2018-12-17 21:47:56 +0800

    add 2 bangumi script
     Tensei Shitara Slime Datta Ken
    Goblin Slayer

---
+++ Goblin_Slayer.py
@@ -0,0 +1,34 @@
+#!/usr/pin/python
+# coding=utf-8
+from __future__ import print_function, unicode_literals
+
+import datetime, re, os, imp
+from bgmi.config import SCRIPT_PATH
+from bgmi.lib.fetch import DATA_SOURCE_MAP
+file, pathname, desc = imp.find_module('script_extend', [os.path.join(SCRIPT_PATH)])
+imp.load_module('script_extend', file, pathname, desc)
+from script_extend.script import SearchScriptBase
+
+
+class Script(SearchScriptBase):
+    class Model(SearchScriptBase.Model):
+        bangumi_name = '哥布林殺手'
+        cover = 'http://lain.bgm.tv/pic/cover/l/c7/2a/238006_OJZy1.jpg'
+        update_time = 'Sat'
+        due_date = datetime.datetime(2019, 1, 15)
+        source = 'dmhy'
+        keyword = '林 杀'
+
+        include_regex_filters = [
+            r'(BIG5|繁体|繁體|\[繁\])',
+        ];
+
+        exclude_regex_filters = [
+            r'(HEVC|MKV|H265)',
+        ];
+    
+
+if __name__ == '__main__':
+    os.environ["TEST_RUN"] = '1'
+    s = Script()
+    print(s.get_download_url())
