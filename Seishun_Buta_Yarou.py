451a71c918a2f54648c9e72823b9e5bcae13e3a2
Skywind <me@iskywind.com>
2018-12-18 22:40:49 +0800

    add some scripts

---
+++ Seishun_Buta_Yarou.py
@@ -0,0 +1,34 @@
+#!/usr/pin/python
+# coding=utf-8
+from __future__ import print_function, unicode_literals
+
+import datetime, re, os, imp
+from bgmi.config import SCRIPT_PATH
+from bgmi.lib.fetch import DATA_SOURCE_MAP
+file, pathname, desc = imp.find_module('script_extend', [os.path.join(SCRIPT_PATH)])
+imp.load_module('script_extend', file, pathname, desc)
+from script_extend.script import SearchScriptBase
+
+
+class Script(SearchScriptBase):
+    class Model(SearchScriptBase.Model):
+        bangumi_name = '青春笨蛋少年不做兔女郎學姐的夢'
+        cover = 'http://lain.bgm.tv/pic/cover/l/b9/45/240038_M7CB5.jpg'
+        update_time = 'Wed'
+        due_date = datetime.datetime(2019, 1, 1)
+        source = 'dmhy'
+        keyword = '兔女郎学姐的梦'
+
+        include_regex_filters = [
+            r'(BIG5|繁体|繁體)',
+        ];
+
+        exclude_regex_filters = [
+            r'(HEVC|MKV|H265)',
+        ];
+    
+
+if __name__ == '__main__':
+    os.environ["TEST_RUN"] = '1'
+    s = Script()
+    print(s.get_download_url())
---
+++ Toaru_Majutsu_no_Index.py
@@ -0,0 +1,65 @@
+#!/usr/pin/python
+# coding=utf-8
+from __future__ import print_function, unicode_literals
+
+import datetime, re
+from bgmi.script import ScriptBase
+from bgmi.lib.fetch import DATA_SOURCE_MAP
+
+TEST_RUN = False
+
+class Script(ScriptBase):
+    class Model(ScriptBase.Model):
+        bangumi_name = '魔法禁書目錄Ⅲ'
+        cover = 'http://lain.bgm.tv/pic/cover/l/be/35/226540_664yS.jpg'
+        update_time = 'Fri'
+        due_date = datetime.datetime(2019, 4, 1)
+        source = 'dmhy'
+        _bangumi_id = '魔法禁書目錄第三季 桜都'
+    
+    def get_download_url(self):
+        """Get the download url, and return a dict of episode and the url.
+        Download url also can be magnet link.
+        For example:
+        ```
+            {
+                1: 'http://example.com/Bangumi/1/1.mp4'
+                2: 'http://example.com/Bangumi/1/2.mp4'
+                3: 'http://example.com/Bangumi/1/3.mp4'
+            }
+        ```
+        The keys `1`, `2`, `3` is the episode, the value is the url of bangumi.
+        :return: dict
+        """
+        if self.source is not None:
+            source = DATA_SOURCE_MAP.get(self.source, None)()
+            if source is None:
+                raise Exception('Script data source is invalid, usable sources: {}'
+                               .format(', '.join(DATA_SOURCE_MAP.keys())))
+            ret = {}
+            data = source.fetch_episode_of_bangumi(**self._data)
+            if TEST_RUN:
+                print(data)
+            for i in data:
+                if TEST_RUN:
+                    print(i['title'])
+                if not re.search(r'(BIG5|繁体|繁體)', i['title']):
+                    if TEST_RUN:
+                        print('filter by cond.1')
+                    continue;
+                if re.search(r'(HEVC|MKV|H265)', i['title']):
+                    if TEST_RUN:
+                        print('filter by cond.2')
+                    continue;
+                if int(i['episode']) not in data:
+                    if TEST_RUN:
+                        print('pass all cond.')
+                    ret[int(i['episode'])] = i['download']
+            return ret
+        else:
+            return {}
+
+if __name__ == '__main__':
+    TEST_RUN = True
+    s = Script()
+    print(s.get_download_url())
