ef89cbe2996086eba728ad46076d6feeeebd034f
Skywind <me@iskywind.com>
2019-04-14 15:41:44 +0800

    [FIX] episode parse

--- Isekai_Quartet.py
+++ Isekai_Quartet.py
@@ -5,6 +5,7 @@ from __future__ import print_function, unicode_literals
 import datetime, re, os, imp
 from bgmi.config import SCRIPT_PATH
 from bgmi.lib.fetch import DATA_SOURCE_MAP
+from bgmi.utils import parse_episode
 file, pathname, desc = imp.find_module('script_extend', [os.path.join(SCRIPT_PATH)])
 imp.load_module('script_extend', file, pathname, desc)
 from script_extend.script import SearchScriptBase
@@ -27,6 +28,31 @@ class Script(SearchScriptBase):
             r'(HEVC|MKV|H265)',
         ];
     
+        post_fetch_hooks = [
+            "post_fetch",
+        ]
+        
+        @classmethod
+        def post_fetch(cls, target, data):
+            for i in data:
+                title = i['title']
+                if i['episode'] != 0:
+                    continue
+                matchs = re.findall(r'([\d]+)', title)
+                if not matchs:
+                    continue
+                for match in matchs:
+                    index = title.index(match)
+                    if index == 0:
+                        continue
+                    title_list = list(title)
+                    title_list.insert(index, ' ')
+                    title = ''.join(title_list)
+                    pass
+                pass
+                episode = parse_episode(title)
+                i['episode'] = episode
+            pass
 
 if __name__ == '__main__':
     os.environ["TEST_RUN"] = '1'
