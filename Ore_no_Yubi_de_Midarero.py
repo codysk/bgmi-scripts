22a4cb54e612174fd4404bc442eb4f210ed87ae5
UmiAoi <ouyuqi@coding.net>
2020-04-24 22:12:45 +0800

    [Update] add bangumi

---
+++ Ore_no_Yubi_de_Midarero.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# coding=utf-8
+from __future__ import print_function, unicode_literals
+
+import datetime, re, os, imp
+from bgmi.config import SCRIPT_PATH
+from bgmi.lib.fetch import DATA_SOURCE_MAP
+file, pathname, desc = imp.find_module('script_extend', [os.path.join(SCRIPT_PATH)])
+imp.load_module('script_extend', file, pathname, desc)
+from script_extend.script import SearchScriptBase
+
+
+class Script(SearchScriptBase):
+    class Model(SearchScriptBase.Model):
+        bangumi_name = '俺の指で乱れろ'
+        cover = 'https://img.skrt.best/images/2020/04/07/96513915272e193cbd9680342bc436e4.png'
+        update_time = 'Sun'
+        # due_date = datetime.datetime(2020, 7, 1)
+        source = 'dmhy'
+        keyword = '手指 扰乱'
+
+        include_regex_filters = [
+            r'(BIG5|繁体|繁體|\[繁\]|繁日)',
+        ];
+
+        exclude_regex_filters = [
+            r'(HEVC|MKV|H265)',
+        ];
+    
+
+if __name__ == '__main__':
+    os.environ["TEST_RUN"] = '1'
+    s = Script()
+    print(s.get_download_url())
---
+++ Shachou_Battle_no_Jikan_desu.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# coding=utf-8
+from __future__ import print_function, unicode_literals
+
+import datetime, re, os, imp
+from bgmi.config import SCRIPT_PATH
+from bgmi.lib.fetch import DATA_SOURCE_MAP
+file, pathname, desc = imp.find_module('script_extend', [os.path.join(SCRIPT_PATH)])
+imp.load_module('script_extend', file, pathname, desc)
+from script_extend.script import SearchScriptBase
+
+
+class Script(SearchScriptBase):
+    class Model(SearchScriptBase.Model):
+        bangumi_name = '社長，戰鬥的時間到了'
+        cover = 'http://lain.bgm.tv/pic/cover/l/03/1f/295707_ih4J0.jpg'
+        update_time = 'Sun'
+        # due_date = datetime.datetime(2020, 7, 1)
+        source = 'dmhy'
+        keyword = '社長 戰鬥 時間'
+
+        include_regex_filters = [
+            r'(BIG5|繁体|繁體|\[繁\]|繁日)',
+        ];
+
+        exclude_regex_filters = [
+            r'(HEVC|MKV|H265)',
+        ];
+    
+
+if __name__ == '__main__':
+    os.environ["TEST_RUN"] = '1'
+    s = Script()
+    print(s.get_download_url())
