dd4de39c731ce7e5ec9944592c08e78c3b4079f4
Skywind <me@iskywind.com>
2021-01-18 23:18:35 +0800

    [Update] add bangumi

---
+++ Ore_dake_Haireru_Kakushi_Dungeon.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# coding=utf-8
+from __future__ import print_function, unicode_literals
+
+import datetime, re, os, imp
+from bgmi.config import SCRIPT_PATH
+from bgmi.lib.fetch import DATA_SOURCE_MAP
+file, pathname, desc = imp.find_module('script_extend', [os.path.join(SCRIPT_PATH)])
+imp.load_module('script_extend', file, pathname, desc)
+from script_extend.script import SearchScriptBase
+
+
+class Script(SearchScriptBase):
+    class Model(SearchScriptBase.Model):
+        bangumi_name = '只有我能進入的隱藏地下城'
+        cover = 'http://lain.bgm.tv/pic/cover/l/fc/73/306147_o4qLV.jpg'
+        update_time = 'Fri'
+        # due_date = datetime.datetime(2021, 4, 1)
+        source = 'dmhy'
+        keyword = '隐藏 地下城'
+
+        include_regex_filters = [
+            r'(BIG5|繁体|繁體|\[繁\]|繁日雙語|CHT)',
+        ];
+
+        exclude_regex_filters = [
+            r'(HEVC|MKV|H265)',
+        ];
+    
+
+if __name__ == '__main__':
+    os.environ["TEST_RUN"] = '1'
+    s = Script()
+    print(s.get_download_url())
---
+++ Tatoeba_Last_Dungeon_Mae_no_Mura_no_Shounen_ga_Joban_no_Machi_de_Kurasu_Youna_Monogatari.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# coding=utf-8
+from __future__ import print_function, unicode_literals
+
+import datetime, re, os, imp
+from bgmi.config import SCRIPT_PATH
+from bgmi.lib.fetch import DATA_SOURCE_MAP
+file, pathname, desc = imp.find_module('script_extend', [os.path.join(SCRIPT_PATH)])
+imp.load_module('script_extend', file, pathname, desc)
+from script_extend.script import SearchScriptBase
+
+
+class Script(SearchScriptBase):
+    class Model(SearchScriptBase.Model):
+        bangumi_name = '比方說，這是個出身魔王關附近的少年在新手村生活的故事'
+        cover = 'http://lain.bgm.tv/pic/cover/l/98/e5/293193_d88nd.jpg'
+        update_time = 'Sun'
+        # due_date = datetime.datetime(2021, 4, 1)
+        source = 'dmhy'
+        keyword = '新手村生活'
+
+        include_regex_filters = [
+            r'(BIG5|繁体|繁體|\[繁\]|繁日雙語|CHT)',
+        ];
+
+        exclude_regex_filters = [
+            r'(HEVC|MKV|H265)',
+        ];
+    
+
+if __name__ == '__main__':
+    os.environ["TEST_RUN"] = '1'
+    s = Script()
+    print(s.get_download_url())
