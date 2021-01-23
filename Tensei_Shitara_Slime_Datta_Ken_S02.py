c3c36217865acd94b322d911ad71c2be02c0a75a
Skywind <me@iskywind.com>
2021-01-23 14:39:22 +0800

    [Update] add Tensei_Slime

---
+++ Tensei_Shitara_Slime_Datta_Ken_S02.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# coding=utf-8
+from __future__ import print_function, unicode_literals
+
+import datetime, re, os, imp
+from bgmi.config import SCRIPT_PATH
+from bgmi.lib.fetch import DATA_SOURCE_MAP
+file, pathname, desc = imp.find_module('script_extend', [os.path.join(SCRIPT_PATH)])
+imp.load_module('script_extend', file, pathname, desc)
+from script_extend.script import SearchScriptBase
+
+
+class Script(SearchScriptBase):
+    class Model(SearchScriptBase.Model):
+        bangumi_name = '關於我轉生變成史萊姆這檔事 第二季'
+        cover = 'http://lain.bgm.tv/pic/cover/l/e6/78/278031_x2NhT.jpg'
+        update_time = 'Tue'
+        # due_date = datetime.datetime(2020, 9, 1)
+        source = 'dmhy'
+        keyword = '史莱姆 第二季'
+
+        include_regex_filters = [
+            r'(BIG5|繁体|繁體|\[繁\]|繁日雙語|CHT)',
+        ];
+
+        exclude_regex_filters = [
+            r'(HEVC|MKV|H265)',
+        ];
+    
+
+if __name__ == '__main__':
+    os.environ["TEST_RUN"] = '1'
+    s = Script()
+    print(s.get_download_url())
