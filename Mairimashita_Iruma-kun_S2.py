2a4100172490461f52bb22478a966259ff2e34f7
Skywind <me@iskywind.com>
2021-05-31 13:15:30 +0800

    [Update] add bangumi Mairimashita! Iruma-kun S2

---
+++ Mairimashita_Iruma-kun_S2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# coding=utf-8
+from __future__ import print_function, unicode_literals
+
+import datetime, re, os, imp
+from bgmi.config import SCRIPT_PATH
+from bgmi.lib.fetch import DATA_SOURCE_MAP
+file, pathname, desc = imp.find_module('script_extend', [os.path.join(SCRIPT_PATH)])
+imp.load_module('script_extend', file, pathname, desc)
+from script_extend.script import SearchScriptBase
+
+
+class Script(SearchScriptBase):
+    class Model(SearchScriptBase.Model):
+        bangumi_name = '入间同学入魔了 第二季'
+        cover = 'https://lain.bgm.tv/pic/cover/l/a4/e8/301740_ZratR.jpg'
+        update_time = 'Tue'
+        # due_date = datetime.datetime(2020, 7, 1)
+        source = 'dmhy'
+        keyword = '入间同学 S2'
+
+        include_regex_filters = [
+            r'(BIG5|繁体|繁體|\[繁\]|繁日雙語|CHT)',
+        ];
+
+        exclude_regex_filters = [
+            r'(HEVC|MKV|H265)',
+        ];
+    
+
+if __name__ == '__main__':
+    os.environ["TEST_RUN"] = '1'
+    s = Script()
+    print(s.get_download_url())
