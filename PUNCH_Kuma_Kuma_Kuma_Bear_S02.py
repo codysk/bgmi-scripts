983d4d7e600af72d5091fa5d73dd19c053532e35
Skywind <me@iskywind.com>
2023-04-05 02:04:33 +0800

    bangumi: kuma punch

---
+++ PUNCH_Kuma_Kuma_Kuma_Bear_S02.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# coding=utf-8
+from __future__ import print_function, unicode_literals
+
+import datetime, re, os, imp
+from bgmi.config import cfg
+from bgmi.lib.fetch import DATA_SOURCE_MAP
+file, pathname, desc = imp.find_module('script_extend', [os.path.join(cfg.script_path)])
+imp.load_module('script_extend', file, pathname, desc)
+from script_extend.script import SearchScriptBase
+
+
+class Script(SearchScriptBase):
+    class Model(SearchScriptBase.Model):
+        bangumi_name = '熊熊勇闖異世界 PUNCH！'
+        cover = 'https://lain.bgm.tv/pic/cover/l/4c/ee/323651_gE7eZ.jpg'
+        update_time = 'Mon'
+        # due_date = datetime.datetime(2023, 7, 1)
+        source = 'dmhy'
+        keyword = '熊熊 PUNCH'
+
+        include_regex_filters = [
+            r'(BIG5|繁体|繁體|\[繁\]|繁日雙語|CHT)',
+        ];
+
+        exclude_regex_filters = [
+            r'(HEVC|MKV|H265)',
+        ];
+    
+
+if __name__ == '__main__':
+    os.environ["TEST_RUN"] = '1'
+    s = Script()
+    print(s.get_download_url())
