72f61b4d049ecd3cb2df355049c645610d813ff4
Skywind <me@iskywind.com>
2023-04-05 01:42:38 +0800

    feature: compatible with bgmi4

--- archived/Mushoku_Tensei_Isekai_Ittara_Honki_Dasu.py
+++ archived/Mushoku_Tensei_Isekai_Ittara_Honki_Dasu.py
@@ -3,9 +3,9 @@
 from __future__ import print_function, unicode_literals
 
 import datetime, re, os, imp
-from bgmi.config import SCRIPT_PATH
+from bgmi.config import cfg
 from bgmi.lib.fetch import DATA_SOURCE_MAP
-file, pathname, desc = imp.find_module('script_extend', [os.path.join(SCRIPT_PATH)])
+file, pathname, desc = imp.find_module('script_extend', [os.path.join(cfg.script_path)])
 imp.load_module('script_extend', file, pathname, desc)
 from script_extend.script import SearchScriptBase
 
