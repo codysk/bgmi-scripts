01a3fb7fca56e8e0488ffc4fd0a334a832222245
Skywind <me@iskywind.com>
2023-04-15 00:17:59 +0800

    bangumi: add bangumi april

---
+++ Kono_Subarashii_Sekai_ni_Bakuen_wo.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# coding=utf-8
+from __future__ import print_function, unicode_literals
+
+import datetime, re, os, imp
+from bgmi.config import cfg
+from bgmi.lib.fetch import DATA_SOURCE_MAP
+file, pathname, desc = imp.find_module('script_extend', [os.path.join(cfg.script_path)])
+imp.load_module('script_extend', file, pathname, desc)
+from script_extend.script import SearchScriptBase
+
+
+class Script(SearchScriptBase):
+    class Model(SearchScriptBase.Model):
+        bangumi_name = '為美好的世界獻上爆焰！'
+        cover = 'https://lain.bgm.tv/pic/cover/l/c7/89/385209_QzpJD.jpg'
+        update_time = 'Wed'
+        # due_date = datetime.datetime(2023, 7, 1)
+        source = 'dmhy'
+        keyword = '美好的世界 爆焰'
+
+        include_regex_filters = [
+            r'(BIG5|繁体|繁體|\[繁\]|繁日雙語|CHT)',
+        ];
+
+        exclude_regex_filters = [
+            r'(HEVC|MKV|H265)',
+        ];
+    
+
+if __name__ == '__main__':
+    os.environ["TEST_RUN"] = '1'
+    s = Script()
+    print(s.get_download_url())
