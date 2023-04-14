ce67300803d926f5482413a2dabd7098335f72f6
Skywind <me@iskywind.com>
2023-04-15 00:34:20 +0800

    bangumi: add bangumi oshi no ko

---
+++ Oshi_no_Ko.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# coding=utf-8
+from __future__ import print_function, unicode_literals
+
+import datetime, re, os, imp
+from bgmi.config import cfg
+from bgmi.lib.fetch import DATA_SOURCE_MAP
+file, pathname, desc = imp.find_module('script_extend', [os.path.join(cfg.script_path)])
+imp.load_module('script_extend', file, pathname, desc)
+from script_extend.script import SearchScriptBase
+
+
+class Script(SearchScriptBase):
+    class Model(SearchScriptBase.Model):
+        bangumi_name = '我推的孩子'
+        cover = 'https://lain.bgm.tv/pic/cover/l/98/5e/386809_1yR81.jpg'
+        update_time = 'Wed'
+        # due_date = datetime.datetime(2023, 7, 1)
+        source = 'dmhy'
+        keyword = '我推的孩子'
+
+        include_regex_filters = [
+            r'(BIG5|繁体|繁體|\[繁\]|繁日雙語|CHT)',
+        ];
+
+        exclude_regex_filters = [
+            r'(HEVC|MKV|H265)',
+        ];
+    
+
+if __name__ == '__main__':
+    os.environ["TEST_RUN"] = '1'
+    s = Script()
+    print(s.get_download_url())
