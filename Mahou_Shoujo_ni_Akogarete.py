71f18818184f9e344bc97ef5c43680a70ed4226c
Skywind <me@iskywind.com>
2024-01-10 23:34:40 +0800

    add bangumi

---
+++ Mahou_Shoujo_ni_Akogarete.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# coding=utf-8
+from __future__ import print_function, unicode_literals
+
+import datetime, re, os, imp
+from bgmi.config import cfg
+from bgmi.lib.fetch import DATA_SOURCE_MAP
+file, pathname, desc = imp.find_module('script_extend', [os.path.join(cfg.script_path)])
+imp.load_module('script_extend', file, pathname, desc)
+from script_extend.script import SearchScriptBase
+
+
+class Script(SearchScriptBase):
+    class Model(SearchScriptBase.Model):
+        bangumi_name = '憧憬成为魔法少女'
+        cover = 'https://lain.bgm.tv/pic/cover/l/96/d1/424663_mM5GN.jpg'
+        update_time = 'Thu'
+        # due_date = datetime.datetime(2024, 4, 1)
+        source = 'dmhy'
+        keyword = '憧憬成为魔法少女'
+
+        include_regex_filters = [
+            r'(BIG5|繁体|繁體|\[繁\]|繁日雙語|繁日|CHT)',
+            r'ANi'
+        ];
+
+        exclude_regex_filters = [
+            r'(HEVC|MKV|H265)',
+        ];
+    
+
+if __name__ == '__main__':
+    os.environ["TEST_RUN"] = '1'
+    s = Script()
+    print(s.get_download_url())
